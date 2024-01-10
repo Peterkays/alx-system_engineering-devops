223b91aba005bb12c706390f56b44f9b1bc4db48
Peter <peterkasinamunda@gmail.com>
2024-01-10 09:00:54 +0200

    removing files

--- 0x14-javascript-web_scraping0/2-statuscode.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-const args = process.argv[2];
-request(args, function (error, response, body) {
-  if (!error) {
-    console.log('code:', response.statusCode);
-  }
-});
--- 0x14-javascript-web_scraping0/3-starwars_title.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-const args = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${args}`;
-request(url, function (error, response, body) {
-  if (!error) {
-    console.log(JSON.parse(body).title);
-  }
-});
